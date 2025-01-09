62c85686dbfe07c49a715f5b8f8ebf395082bfd0
seminnumanovic <semin.contact@gmail.com>
2025-01-09 04:17:05 +0100

    test

--- Javasc-main/To_Do_List/script.js
+++ Javasc-main/To_Do_List/script.js
@@ -1,16 +1,27 @@
-function add() {
-    let inputElement = document.getElementById('input');
-    let inputText = inputElement.value.trim();
-    if (inputText !== '') {
-        let listElement = document.createElement('li');
-        let textNode = document.createTextNode(inputText);
-        listElement.appendChild(textNode);
-        document.querySelector('#tasks').appendChild(listElement);
-        inputElement.value = '';
-        let removeBtn = document.createElement('button');
-        function remove(){
-            
-        }
-        removeBtn.addEventListener('click', remove)
-    }
+let list = document.createElement('ul');
+list.className = 'ul';
+let main = document.querySelector('.main').appendChild(list);
+
+
+function addQuest(){
+    let quest = document.querySelector('#quest').value;
+    let newQest = document.createElement('li'); 
+    let removeButton = document.createElement('button');
+    removeButton.className = 'removeButton';
+    removeButton.innerText = `x`;
+    removeButton.addEventListener('click',removeButtonFunc);
+    
+    let container = document.createElement('div');
+    container.className = 'container';
+    container.appendChild(newQest);
+    container.appendChild(removeButton);
+    list.appendChild(container);  
+
+    newQest.innerHTML = quest;
+    newQest.className = 'li';
+}
+
+function removeButtonFunc(event) {
+    let container = event.target.parentElement;
+    list.removeChild(container);
 }
